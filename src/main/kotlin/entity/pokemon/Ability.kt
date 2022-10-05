8de5fadb776cca1c35aa24711a6506a339d15558
Tykok <treportelie12@gmail.com>
2022-10-05 22:50:16 +0200

    Feature #1 - Add "Ability" classes according to PokeApi

--- src/main/kotlin/entity/pokemon/Ability.kt
+++ src/main/kotlin/entity/pokemon/Ability.kt
@@ -1,4 +1,61 @@
 package entity.pokemon
 
-class Ability {
-}
+import entity.common.Name
+import entity.common.NamedApiResource
+import entity.common.VerboseEffect
+import entity.games.Generation
+
+class Ability (
+
+    /**
+    * The identifier for this resource.
+    */
+    val id: Number,
+
+    /**
+    * The name for this resource.
+    */
+    val name: String,
+
+    /**
+    * Whether or not this ability originated in the main series of the video games.
+    */
+    val is_main_series: Boolean,
+
+    /**
+    * The generation this ability originated in.
+     * @see Generation
+     * @see NamedApiResource
+    */
+    val generation: NamedApiResource,
+
+    /**
+    * The name of this resource listed in different languages.
+     * @see Name
+    */
+    val names: Array<Name>,
+
+    /**
+    * The effect of this ability listed in different languages.
+     * @see VerboseEffect
+    */
+    val effect_entries: Array<VerboseEffect>,
+
+    /**
+    * The list of previous effects this ability has had across version groups.
+     * @see AbilityEffectChange
+    */
+    val effect_changes: Array<AbilityEffectChange>,
+
+    /**
+    * The flavor text of this ability listed in different languages.
+     * @see AbilityFlavorText
+    */
+    val flavor_text_entries: Array<AbilityFlavorText>,
+
+    /**
+    * A list of Pokémon that could potentially have this ability.
+     * @see AbilityPokemon
+    */
+    val pokemon: Array<AbilityPokemon>
+)
--- src/main/kotlin/entity/pokemon/AbilityPokemon.kt
+++ src/main/kotlin/entity/pokemon/AbilityPokemon.kt
@@ -1,4 +1,23 @@
 package entity.pokemon
 
-class AbilityPokemon {
-}
+import entity.common.NamedApiResource
+
+class AbilityPokemon(
+
+    /**
+     * Whether or not this a hidden ability for the referenced Pokémon.
+     */
+    val is_hidden: Boolean,
+
+    /**
+     * Pokémon have 3 ability 'slots' which hold references to possible abilities they could have. This is the slot of this ability for the referenced pokemon.
+     */
+    val slot: Number,
+
+    /**
+     * The Pokémon this ability could belong to.
+     * @see Pokemon
+     * @see NamedApiResource
+     */
+    val pokemon: NamedApiResource
+)
