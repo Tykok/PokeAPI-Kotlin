249485ad7c86ba66dc73fccbd1a859ef639235a1
Tykok <treportelie12@gmail.com>
2022-10-05 22:50:35 +0200

    Feature #1 - Add "EggGroup" classes according to PokeApi

--- src/main/kotlin/entity/pokemon/EggGroup.kt
+++ src/main/kotlin/entity/pokemon/EggGroup.kt
@@ -1,4 +1,29 @@
 package entity.pokemon
 
-class EggGroup {
-}
+import entity.common.Name
+import entity.common.NamedApiResource
+
+class EggGroup(
+    /**
+     * The identifier for this resource.
+     */
+    val id: Number,
+
+    /**
+     * The name for this resource.
+     */
+    val name: String,
+
+    /**
+     * The name of this resource listed in different languages.
+     * @see Name
+     */
+    val names: Array<Name>,
+
+    /**
+     * A list of all Pokémon species that are members of this egg group.
+     * @see PokemonSpecies
+     * @see NamedApiResource
+     */
+    val pokemon_species: Array<NamedApiResource>
+)
