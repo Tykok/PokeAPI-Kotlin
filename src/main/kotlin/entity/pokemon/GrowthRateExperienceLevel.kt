18773915ac9f207082343c6f7bb53124bd6fd444
Tykok <treportelie12@gmail.com>
2022-10-09 00:43:33 +0200

    Feature #1 - Add "Growth" classes according to PokeApi

--- src/main/kotlin/entity/pokemon/GrowthRateExperienceLevel.kt
+++ src/main/kotlin/entity/pokemon/GrowthRateExperienceLevel.kt
@@ -1,4 +1,15 @@
 package entity.pokemon
 
-class GrowthRateExperienceLevel {
-}
+class GrowthRateExperienceLevel(
+
+    /**
+     * The level gained.
+     */
+    val level: Number,
+
+
+    /**
+     * The amount of experience required to reach the referenced level.
+     */
+    val experience: Number
+)
