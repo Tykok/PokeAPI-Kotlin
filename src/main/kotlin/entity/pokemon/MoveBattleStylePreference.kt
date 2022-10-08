59f7407e6ca5fa8128f394ffb09425c7b0cd32e9
Tykok <treportelie12@gmail.com>
2022-10-09 00:43:57 +0200

    Feature #1 - Add "Natures" classes according to PokeApi

--- src/main/kotlin/entity/pokemon/MoveBattleStylePreference.kt
+++ src/main/kotlin/entity/pokemon/MoveBattleStylePreference.kt
@@ -1,4 +1,24 @@
 package entity.pokemon
 
-class MoveBattleStylePreference {
-}
+import entity.common.NamedApiResource
+import entity.moves.MoveBattleStyle
+
+class MoveBattleStylePreference(
+    /**
+     * Chance of using the move, in percent, if HP is under one half.
+     */
+    val low_hp_preference: Number,
+
+    /**
+     * Chance of using the move, in percent, if HP is over one half.
+     */
+    val high_hp_preference: Number,
+
+    /**
+     * The move battle style.
+     * @see MoveBattleStyle
+     * @see NamedApiResource
+     */
+    val move_battle_style: NamedApiResource
+
+)
--- src/main/kotlin/entity/pokemon/Nature.kt
+++ src/main/kotlin/entity/pokemon/Nature.kt
@@ -1,4 +1,65 @@
 package entity.pokemon
 
-class Nature {
-}
+import entity.common.Name
+import entity.common.NamedApiResource
+import entity.berries.BerryFlavor
+
+class Nature(
+
+    /**
+     * The identifier for this resource.
+     */
+    val id: Number,
+
+    /**
+     * The name for this resource.
+     */
+    val name: String,
+
+    /**
+     * The stat decreased by 10% in Pokémon with this nature.
+     * @see Stat
+     * @see NamedApiResource
+     */
+    val decreased_stat: NamedApiResource,
+
+    /**
+     * The stat increased by 10% in Pokémon with this nature.
+     * @see Stat
+     * @see NamedApiResource
+     */
+    val increased_stat: NamedApiResource,
+
+    /**
+     * The flavor hated by Pokémon with this nature.
+     * @see BerryFlavor
+     * @see NamedApiResource
+     */
+    val hates_flavor: NamedApiResource,
+
+    /**
+     * The flavor liked by Pokémon with this nature.
+     * @see BerryFlavor
+     * @see NamedApiResource
+     */
+    val likes_flavor: NamedApiResource,
+
+    /**
+     * A list of Pokéathlon stats this nature effects and how much it effects them.
+     * @see NatureStatChange
+     */
+    val pokeathlon_stat_changes: Array<NatureStatChange>,
+
+    /**
+     * A list of battle styles and how likely a Pokémon with this nature is to use them in the Battle Palace or Battle Tent.
+     * @see MoveBattleStylePreference
+     */
+    val move_battle_style_preferences: Array<MoveBattleStylePreference>,
+
+    /**
+     * The name of this resource listed in different languages.
+     * @see Name
+     */
+    val names: Array<Name>
+
+)
